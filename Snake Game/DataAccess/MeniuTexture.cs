986d62bd7d487e91da411a744b4690f239a47005
vytas_cp <SND\vytas_cp>
2012-05-01 09:42:06 +0000

    Meniu sutvarkytas

--- Snake Game/DataAccess/MeniuTexture.cs
+++ Snake Game/DataAccess/MeniuTexture.cs
@@ -8,6 +8,9 @@ namespace DataAccess
 {
     public class MeniuTexture
     {
+            /// <summary>
+            /// Pagrindinio meniu tekstūros.
+            /// </summary>
             public Texture2D background { set; get; }
             public Texture2D gameTitle { set; get; }
             public Texture2D meniu { set; get; }
@@ -21,6 +24,9 @@ namespace DataAccess
             public Texture2D sHighscores { set; get; }
             public Texture2D sHighscores_marked { set; get; }
 
+            /// <summary>
+            ///Žaidimo meniu pasirinkimo tekstūros. 
+            /// </summary>
             public Texture2D gameTypeTitle { set; get; }
             public Texture2D sClassical { set; get; }
             public Texture2D sClassical_marked { set; get; }
@@ -28,6 +34,11 @@ namespace DataAccess
             public Texture2D sArcade_marked { set; get; }
             public Texture2D sBack { set; get; }
             public Texture2D sBack_marked { set; get; }
+            
+            /// <summary>
+            /// Žaidimo sudėtingumo pasirinkimo tekstūros.
+            /// </summary>
+            public Texture2D diffLevelTitle { set; get; }
             public Texture2D sEasy { set; get; }
             public Texture2D sEasy_marked { set; get; }
             public Texture2D sMedium { set; get; }
@@ -35,6 +46,9 @@ namespace DataAccess
             public Texture2D sHard { set; get; }
             public Texture2D sHard_marked { set; get; }
 
+            /// <summary>
+            /// Gyvatės pasirinkimo tekstūros
+            /// </summary>
             public Texture2D chooseSnakeTitle { set; get; }
             public Texture2D sSnake1 { set; get; }
             public Texture2D sSnake1_marked { set; get; }
@@ -46,6 +60,9 @@ namespace DataAccess
             public Texture2D bTreeSnake2 { set; get; }
             public Texture2D bTreeSnake3 { set; get; }
 
+            /// <summary>
+            /// Nuotykių rėžimo lygio pasirinkimo tekstūros.
+            /// </summary>
             public Texture2D sLevel1 { set; get; }
             public Texture2D sLevel1Marked { set; get; }
             public Texture2D sLevel2 { set; get; }
@@ -64,6 +81,9 @@ namespace DataAccess
             public Texture2D sBackSMarked { set; get; }
             public Texture2D darkSignPole { set; get; }
 
+            /// <summary>
+            /// Pasiekimų lentos tekstūros.
+            /// </summary>
             public Texture2D highScoresArc { set; get; }
             public Texture2D highScoresCla { set; get; }
             public Texture2D bNext { set; get; }
@@ -71,6 +91,9 @@ namespace DataAccess
             public Texture2D sBack2 { set; get; }
             public Texture2D sBack2Marked { set; get; }
 
+            /// <summary>
+            /// Pagalbos lango tekstūros.
+            /// </summary>
             public Texture2D help { set; get; }
             public Texture2D bDown { set; get; }
             public Texture2D bDownMarked { set; get; }
