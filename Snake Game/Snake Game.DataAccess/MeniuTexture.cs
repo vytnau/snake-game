2e2081b6ff3d23d3bbb112f0d32e2865a8ca488d
vytas_cp <SND\vytas_cp>
2012-05-12 20:24:21 +0000

    Veikenti versija

---
+++ Snake Game/Snake Game.DataAccess/MeniuTexture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DataAccess
+{
+    public class MeniuTexture
+    {
+            public Texture2D background { set; get; }
+            public Texture2D gameTitle { set; get; }
+            public Texture2D meniu { set; get; }
+            public Texture2D new_game { set; get; }
+            public Texture2D quit { set; get; }
+    }
+}
