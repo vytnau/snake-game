a87ba001f99fe46d8dfc5bdc06619a3f6dda6f06
Vytautas <3790809+vytnau@users.noreply.github.com>
2012-06-04 16:01:14 +0000

    Sukurtos klases: MeniuSound meniuSoundService zodziu bandymas ant greitu paleisti garsus, kolkas nieko gero.

---
+++ Snake Game/DomainModel/Sound/MeniuSound.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+
+namespace DomainModel.Sound
+{
+    public class MeniuSound
+    {
+        SoundEffectInstance soundEngineInstance;
+        public SoundEffect SoundArrowCrackle { set; get; }
+        public void Creat()
+        {
+            soundEngineInstance = SoundArrowCrackle.CreateInstance();
+        }
+
+        public void Play()
+        {
+            soundEngineInstance.Volume = 0.75f;
+            //soundEngineInstance.IsLooped = true;
+            soundEngineInstance.Play();
+        }
+
+        
+    }
+}
