e46f1a63de75940f61d4f7086f4c031d82513017
Vytautas <3790809+vytnau@users.noreply.github.com>
2013-11-11 10:36:20 +0000

    Sukurta testu zaidejo servisui.

--- Snake Game/SnakeGame.Test/PlayerServiceTest.cs
+++ Snake Game/SnakeGame.Test/PlayerServiceTest.cs
@@ -2,11 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
+using Snake_Game.ServiceContracts;
+using Snake_Game.Service;
 
-namespace SnakeGame.Test
+namespace SnakeGame.GameTest
 {
-    [Test]
+    [TestFixture]
     public class PlayerServiceTest
     {
+        [Test]
+        public void Add10Points()
+        {
+            IPlayerService playerService = new PlayerService();
+            playerService.AddPoint(10);
+            Assert.AreEqual(10, playerService.GetPoints());
+        }
+
+        [Test]
+        public void AddMinus10Points()
+        {
+            IPlayerService playerService = new PlayerService();
+            playerService.AddPoint(-10);
+            Assert.AreEqual(-10, playerService.GetPoints());
+        }
+
+        [Test]
+        public void AddMinus1Live()
+        {
+            IPlayerService playerService = new PlayerService();
+            playerService.DecreseLive();
+            Assert.AreEqual(2, playerService.GetLive());
+        }
+
+        [Test]
+        public void AddMinus4Live()
+        {
+            IPlayerService playerService = new PlayerService();
+            playerService.DecreseLive();
+            playerService.DecreseLive();
+            playerService.DecreseLive();
+            playerService.DecreseLive();
+            Assert.AreEqual(0, playerService.GetLive());
+        }
     }
 }
